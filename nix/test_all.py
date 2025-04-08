61dbcb840e97be70ffd2f6d736badaad4e831d89
quapka <quapka@gmail.com>
2025-04-08 10:45:41 +0200

    Autoformat

--- nix/test_all.py
+++ nix/test_all.py
@@ -31,14 +31,27 @@ def library_name(library):
         case "mbedtls":
             return "mbedTLS"
 
+
 def base_options(library):
     match library:
-        case "openssl" | "botan" | "boringssl" | "ippcp" | "libressl" | "gcrypt" | "nettle":
-            return ["-ps", "123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234"]
+        case (
+            "openssl"
+            | "botan"
+            | "boringssl"
+            | "ippcp"
+            | "libressl"
+            | "gcrypt"
+            | "nettle"
+        ):
+            return [
+                "-ps",
+                "123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234",
+            ]
         case "cryptopp" | "mbedtls":
             return ["-ps", "12345678"]
         case _:
             return []
+
 
 def default_options(library):
     match library:
@@ -47,82 +60,117 @@ def default_options(library):
         case _:
             return []
 
+
 def test_vectors_options(library):
     return default_options(library)
+
 
 def performance_options(library):
     return default_options(library)
 
+
 def signature_options(library):
     match library:
-        case "nettle" | "gcrypt" | "boringssl" | "openssl" | "tomcrypt" | "libressl" | "ippcp" | "mbedtls":
+        case (
+            "nettle"
+            | "gcrypt"
+            | "boringssl"
+            | "openssl"
+            | "tomcrypt"
+            | "libressl"
+            | "ippcp"
+            | "mbedtls"
+        ):
             return ["-st", "NONEwithECDSA"]
         case _:
             return []
 
+
 def miscellaneous_options(library):
     return default_options(library)
+
 
 def twist_options(library):
     return default_options(library)
 
+
 def invalid_options(library):
     return default_options(library)
+
 
 def degenerate_options(library):
     return default_options(library)
 
+
 def cofactor_options(library):
     return default_options(library)
+
 
 def composite_options(library):
     return default_options(library)
 
+
 def edge_cases_options(library):
     return default_options(library)
+
 
 def wrong_options(library):
     return default_options(library)
 
+
 def build_library(library, version):
-    command = ["nix", "build", "--log-format", "raw", f"?submodules=1#{library}.{version}"]
+    command = [
+        "nix",
+        "build",
+        "--log-format",
+        "raw",
+        f"?submodules=1#{library}.{version}",
+    ]
     result = sp.run(command, check=False)
     print(f"build {library} {version} = {result.returncode}")
     return result.returncode == 0
+
 
 def test_library(library, test_suite, version):
     opts = base_options(library)
     opts.extend(globals()[f"{test_suite.replace('-', '_')}_options"](library))
     lib_name = library_name(library)
-    command = ["./result/bin/ECTesterStandalone", "test",
-               f"-oyml:results/yml/{library}_{test_suite}_{version}.yml",
-               f"-otext:results/txt/{library}_{test_suite}_{version}.txt",
-               f"-oxml:results/xml/{library}_{test_suite}_{version}.xml",
-               "-q", *opts, test_suite, lib_name]
+    command = [
+        "./result/bin/ECTesterStandalone",
+        "test",
+        f"-oyml:results/yml/{library}_{test_suite}_{version}.yml",
+        f"-otext:results/txt/{library}_{test_suite}_{version}.txt",
+        f"-oxml:results/xml/{library}_{test_suite}_{version}.xml",
+        "-q",
+        *opts,
+        test_suite,
+        lib_name,
+    ]
     try:
         result = sp.run(command, timeout=60, check=False)
         print(f"run {library} {test_suite} {version} = {result.returncode}")
     except sp.TimeoutExpired:
         print(f"run {library} {test_suite} {version} timed-out!")
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--library")
-    parser.add_argument("-s", "--suite")
+    parser.add_argument("-l", "--library", help="The name of the library to test.")
+    parser.add_argument("-s", "--suite", help="The ECTester test suite to run.")
     args = parser.parse_args()
     library = args.library
     suite = args.suite
 
     libraries = [
-         "botan", 
-         "cryptopp", 
-         "openssl", 
-         "boringssl", 
-         "gcrypt", 
-         "mbedtls", 
-         "ippcp", 
-         "nettle", 
-         "libressl", 
+        "botan",
+        "cryptopp",
+        "openssl",
+        "boringssl",
+        "gcrypt",
+        "mbedtls",
+        "ippcp",
+        "nettle",
+        "libressl",
     ]
 
     suites = [
@@ -137,9 +185,8 @@ def main():
         "edge-cases",
         "cofactor",
         "composite",
-        "wrong"
+        "wrong",
     ]
-
 
     if library is None:
         libraries2test = libraries
@@ -165,5 +212,5 @@ def main():
                     test_library(library, suite, version)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
